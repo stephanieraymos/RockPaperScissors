e8252edad7047bdc115a20befeb014c3cfb96b51
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-08-19 16:40:33 -0700

    Selections printing properly to screen

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const selectionButton = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column')
 const SELECTIONS = [
     {
         name: 'rock',
@@ -29,7 +30,22 @@ selectionButton.forEach(selectionButton => {
 
 function makeSelection(selection) {
     const computerSelection = randomSelection()
-    console.log(computerSelection)
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats  === opponentSelection.name
 }
 
 function randomSelection() {
