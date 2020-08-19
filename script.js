33844ffedbcf67a2e2cf4ae70c60282c4af78c52
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-08-19 16:19:35 -0700

    Created variable to store all possible selections

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '👊',
+        beats: 'scissors'
 
+    },
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+
+    },
+    {
+        name: 'scissors',
+        emoji: '✌',
+        beats: 'paper'
+
+    }
+]
 selectionButtons.forEach(selectionButtons => {
     selectionButtons.addEventListener('click', e => {
         const selectionName = selectionButtons.dataset.selection
